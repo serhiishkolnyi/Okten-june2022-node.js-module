f36e970b75ee0ba52cc9cdefdedd709e9edfb80f
SergiyShkolniy <s.shkolniy@gmail.com>
2022-11-11 09:23:09 +0200

    optimization code

--- homework-01/app.js
+++ homework-01/app.js
@@ -1,6 +1,5 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
-
 
 const girls = [
     {name: 'Olena', age: 39, gender: 'female'},
@@ -17,135 +16,29 @@ const boys = [
     {name: 'Nadiya', age: 61, gender: 'female'},
 ];
 
+const createAndSortStudents = async (pathMain, array, readFolder, writeFolder, gender) => {
+    try {
+        await fs.mkdir(path.join(__dirname, pathMain), {recursive: true});
+        await fs.mkdir(path.join(__dirname, pathMain, readFolder));
 
-function createDirectoryStudents() {
-    return new Promise((resolve, reject) => {
-         fs.mkdir(path.join(__dirname, 'students'), (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                resolve ('#1. create directory students')
+        for (const student of array) {
+            await fs.appendFile(path.join(__dirname, pathMain, readFolder, `${student.name}.json`), JSON.stringify(student));
+        }
+
+        const files = await fs.readdir(path.join(__dirname, pathMain, readFolder));
+
+        for (const file of files) {
+            const data = await fs.readFile(path.join(__dirname, pathMain, readFolder, file));
+            const studentInfo = await JSON.parse(data);
+
+            if (studentInfo.gender === gender) {
+                await fs.rename(path.join(__dirname, pathMain, readFolder, file),path.join(__dirname, pathMain, writeFolder, file))
             }
-        });
-    })
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-function createFolderBoys() {
-    return new Promise((resolve, reject) => {
-        fs.mkdir(path.join(__dirname, 'students', 'boys'), (err) => {
-            if (err) {
-                console.log(err);
-                reject('errors boys')
-            } else {
-                boys.forEach(boy => {
-                    fs.appendFile(path.join(__dirname, 'students', 'boys', `${boy.name}.json`),
-                        `${JSON.stringify(boy)}`, (err) => {
-                            if (err) {
-                                console.log(err);
-                                throw err;
-                            } else {
-                               resolve('#2. create folders boys');
-                            }
-                        });
-                        
-                });
-            }
-        })
-    });
-}
-
-function createFolderGirls() {
-    return new Promise((resolve, reject) => {
-        fs.mkdir(path.join(__dirname, 'students', 'girls'), (err) => {
-            if (err) {
-                console.log(err);
-                reject('errors girls')
-            }else {
-                girls.forEach(girl => {
-                    fs.appendFile(path.join(__dirname, 'students', 'girls', `${girl.name}.json`),
-                        `${JSON.stringify(girl)}`, (err) => {
-                            if (err) {
-                                console.log(err);
-                                throw err;
-                            }
-                            resolve('#3. create folders girls');
-                        });
-                });
-            }
-        })
-    })
-}
-
-
-const sortStudent = () => {
-    return new Promise((resolve, reject) => {
-        fs.readdir(path.join(__dirname, 'students', 'boys'), {withFileTypes: true}, (err, files) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }else {
-                for (let file of files){
-                    if(file.isFile){
-                        fs.readFile(path.join(__dirname, 'students', 'boys', `${file.name}`), (err, data) =>{
-                            const gender = JSON.parse(data.toString()).gender
-                            if(gender === 'female'){
-                                fs.rename(path.join(__dirname, 'students', 'boys', `${file.name}`), path.join(__dirname, 'students', 'girls', `${file.name}`), (err, data)=>{
-                                    if (err) {
-                                        console.log(err);
-                                        throw err;
-                                    }else {
-                                        resolve('#4 . girls to girls')
-                                    }
-                                })
-                            }
-                        })
-                    }
-                }
-            }
-            
-        })
-
-        fs.readdir(path.join(__dirname, 'students', 'girls'), {withFileTypes: true}, (err, files) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }else {
-                for (let file of files){
-                    if(file.isFile){
-                        fs.readFile(path.join(__dirname, 'students', 'girls', `${file.name}`), (err, data) =>{
-                            const gender = JSON.parse(data.toString()).gender
-                            if(gender === 'male'){
-                                fs.rename(path.join(__dirname, 'students', 'girls', `${file.name}`), path.join(__dirname, 'students', 'boys', `${file.name}`), (err, data)=>{
-                                    if (err) {
-                                        console.log(err);
-                                        throw err;
-                                    }else {
-                                        resolve('#4 . boys to boys and girls to girs')
-                                    }
-                                })
-                            }
-                        })
-                    }
-                }
-            }
-
-        })
-    })
-}
-
-async function start() {
-   const students =  await createDirectoryStudents();
-   const boys = await createFolderBoys();
-   const girls = await createFolderGirls();
-   const sort = await sortStudent();
-
-    console.log(students);
-    console.log(boys);
-    console.log(girls);
-    console.log(sort);
-}
-
-start()
-
-
-
+createAndSortStudents('students', boys, 'boys', 'girls', 'female');
+createAndSortStudents('students', girls, 'girls', 'boys', 'male');
